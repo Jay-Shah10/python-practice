ea93a29426c76466939666672a6bcef2126e56de
Jay Shah <j24shah@gmail.com>
2022-05-03 22:57:39 -0400

    getting keys

--- hash-tables/hashtable.py
+++ hash-tables/hashtable.py
@@ -22,6 +22,14 @@ class HashTable:
                     return self.data_map[index][i][1] # this 1 is the value that is at the key, which is what we want to return. 
         return None
 
+    def keys(self):
+        all_keys = []
+        for i in range(len(self.data_map)):
+            if self.data_map[i] is not None: 
+                for j in range(len(self.data_map[i])):
+                    all_keys.append(self.data_map[i][j][0])
+        return all_keys
+
     
     
     
@@ -39,6 +47,9 @@ def main():
     print()
 
     print(table.get_item('bolts'))
+    print()
+
+    print(table.keys())
     
 
 
